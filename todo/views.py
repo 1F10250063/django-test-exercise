f166b25c3fee63fda51adb0d9944e4cb94d0cce6
1f10250063 <s1f102500630@iniad.org>
2026-06-25 13:54:31 +0900

    警告部分を改善2

--- todo/views.py
+++ todo/views.py
@@ -7,7 +7,7 @@ from todo.models import Task
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        task = Task(title=request.POST['title'],due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
     
     if request.GET.get('order') == 'due':
@@ -17,7 +17,7 @@ def index(request):
     
 
     context = {
-        'tasks' : tasks
+        'tasks': tasks
     }
-    return render(request,'todo/index.html',context)
+    return render(request, 'todo/index.html', context)
     
